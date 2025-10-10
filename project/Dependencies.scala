9fc29859922b9f85d8ae3858fcd9cc183c4a358c
Krzysztof Atlasik <krzysztof.atlasik@protonmail.com>
2025-10-10 14:01:23 +0200

    Bump library versions. Update sbt.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt.*
 object Dependencies {
 
   val Examples = Seq(
-    "com.softwaremill.sttp.tapir" %% "tapir-core" % "1.11.44",
-    "com.softwaremill.sttp.tapir" %% "tapir-netty-server-sync" % "1.11.44",
-    "com.softwaremill.ox" %% "core" % "1.0.0",
+    "com.softwaremill.sttp.tapir" %% "tapir-core" % "1.11.48",
+    "com.softwaremill.sttp.tapir" %% "tapir-netty-server-sync" % "1.11.48",
+    "com.softwaremill.ox" %% "core" % "1.0.1",
     "org.typelevel" %% "cats-effect" % "3.6.3"
   )
 
