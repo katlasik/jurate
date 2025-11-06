fd6b14266cda8dd30986c15884fc124bd0877999
Krzysztof Atlasik <krzysztof.atlasik@protonmail.com>
2025-11-06 20:34:15 +0100

    Fix erroneous statements in README.md

--- examples/src/main/scala/example/catsEffect.scala
+++ examples/src/main/scala/example/catsEffect.scala
@@ -5,7 +5,8 @@ import jurate.{*, given}
 
 case class AppConfig(
     @env("APP_NAME") name: String = "CatsEffectApp",
-    @env("APP_VERSION") version: String
+    @env("APP_VERSION") version: String,
+    @env("USERS") users: Vector[String] = Vector.empty
 )
 
 object CatsEffect extends IOApp.Simple {
